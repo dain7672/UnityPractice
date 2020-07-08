48055a855ad72607d60bfba5307871234b0b6465
DAIN KIM <dain7672@gmail.com>
2020-07-08 22:59:12 +0900

    이것저것 해봄.. + motion particle을 avatar에 넣어봄.

---
+++ Assets/01_Motion/ChildObjectCheckScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildObjectCheckScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            //child is your child transform
+            Debug.Log(child);
+        }
+        Debug.Log(transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).gameObject);
+        transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).gameObject.transform.localPosition = new Vector3(3,3,3);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/01_Motion/MotionAnimationRootScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+    
+public class MotionAnimationRootScript : MonoBehaviour {
+
+    HumanPoseHandler humanPoseHandler;
+	HumanPose humanPose;
+	Animator anim;
+
+    Transform head;
+            
+    // void OnAnimatorMove()
+    // {
+    //         Animator animator = GetComponent<Animator>(); 
+                              
+    //         if (animator)
+    //         {
+    //             Vector3 newPosition = transform.position;
+    //            newPosition.z += Time.deltaTime; 
+    //  transform.position = newPosition;
+    //             float legPosition = 0;
+    //             legPosition += Time.deltaTime;
+    //             animator.SetFloat("LeftUpperLegIn-Out", legPosition); 
+    //         }
+    // }
+
+    void Start() {
+        LookUpMuscleIndex();
+    }
+    void Update() {
+        anim = GetComponent<Animator>();
+
+		// run this if you want the indexes to muscles on your character
+		
+
+		humanPoseHandler = new HumanPoseHandler(anim.avatar, transform);
+		// get human pose
+		humanPose = new HumanPose();
+
+		// TODO keeping body above plane
+		//humanPose.bodyPosition = current_position;
+
+		// reference pose to pose handler
+		humanPoseHandler.GetHumanPose(ref humanPose);
+
+		// set a specific musle; 9: Neck Nod Down-Up
+        humanPose.muscles[9] = 40f - (40f * Time.deltaTime * 40f); 
+        Debug.Log(humanPose.muscles[9]);
+
+        // use pose information to actually set the pose; doesn't work so far
+        humanPoseHandler.SetHumanPose(ref humanPose);
+    }
+
+    void LookUpMuscleIndex() {
+		string[] muscleName = HumanTrait.MuscleName;
+		int i = 0;
+		while (i < HumanTrait.MuscleCount)
+		{
+			Debug.Log(i + ": " + muscleName[i] +
+				" min: " + HumanTrait.GetMuscleDefaultMin(i) + " max: " + HumanTrait.GetMuscleDefaultMax(i));
+			i++;
+		}
+	}
+}
