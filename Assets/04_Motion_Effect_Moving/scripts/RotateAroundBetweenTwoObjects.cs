774591db2066b31ff3904be470e8d3538e59789d
DAIN KIM <dain7672@gmail.com>
2020-07-11 21:32:14 +0900

    rotate beween two objects

---
+++ Assets/04_Motion_Effect_Moving/scripts/RotateAroundBetweenTwoObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundBetweenTwoObjects : MonoBehaviour
+{
+    public GameObject object1;
+    public GameObject object2;
+    private Vector3 position;
+    private Vector3 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.Lerp(object1.transform.position, object2.transform.position, 0.5f);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 0.2f, transform.localPosition.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = Vector3.Lerp(object1.transform.position, object2.transform.position, 0.5f);
+        vector =  object1.transform.position - object2.transform.position;
+
+        Debug.Log(vector);
+        transform.RotateAround(position, vector, 4);
+
+    }
+}
