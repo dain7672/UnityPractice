0090b7db0718369a5b2b80272e731f2ddffbab33
DAIN KIM <dain7672@gmail.com>
2020-07-11 20:59:20 +0900

    glowing effect rotate around cylinder arm in dummy humanoid walking

---
+++ Assets/04_Motion_Effect_Moving/scripts/MoveBetweenTwoObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetweenTwoObjects : MonoBehaviour
+{
+    public GameObject obj;
+    public GameObject particle2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(obj.transform.position, particle2.transform.position, 0.5f);
+    }
+}
