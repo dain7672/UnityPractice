7b74ece4172016052239525b4aca1488f73ddfd2
DAIN KIM <dain7672@gmail.com>
2020-07-15 23:06:03 +0900

    toggle 부분 구현

---
+++ Assets/04_Motion_Effect_Moving/scripts/SkinVisibleByToggle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinVisibleByToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool isOn = PlayerPrefs.GetInt("isSkinVisible") == 1;
+        GetComponent<SkinnedMeshRenderer>().enabled = isOn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isOn = PlayerPrefs.GetInt("isSkinVisible") == 1;
+        GetComponent<SkinnedMeshRenderer>().enabled = isOn;
+    }
+}
---
+++ Assets/04_Motion_Effect_Moving/scripts/ToggleElements.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleElements : MonoBehaviour
+{
+    public Toggle skinToggle;
+    public Toggle skeletonToggle;
+    public Toggle effectToggle;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("isSkinVisible", 1);
+        PlayerPrefs.SetInt("isSkeletonVisible", 1);
+        PlayerPrefs.SetInt("isEffectVisible", 1);
+    }
+    public void toggleSkin()
+    {
+        PlayerPrefs.SetInt("isSkinVisible", skinToggle.isOn ? 1 : 0);
+    }
+
+    public void toggleSkeleton()
+    {
+        PlayerPrefs.SetInt("isSkeletonVisible", skeletonToggle.isOn ? 1 : 0);
+    }
+
+    public void toggleEffect()
+    {
+        PlayerPrefs.SetInt("isEffectVisible", effectToggle.isOn ? 1 : 0);
+    }
+}
