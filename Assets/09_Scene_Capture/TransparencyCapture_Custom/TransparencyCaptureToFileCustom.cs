7bf2e513014fbb00b79d9405771b512b1fbc1458
DAIN KIM <dain7672@gmail.com>
2020-07-23 23:11:04 +0900

    add object capturing asset. Add Capturing Test

---
+++ Assets/09_Scene_Capture/TransparencyCapture_Custom/TransparencyCaptureToFileCustom.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class TransparencyCaptureToFileCustom:MonoBehaviour
+{
+    private int currentFrame = 0;
+    public IEnumerator capture(int currentFrame)
+    {
+
+        yield return new WaitForEndOfFrame();
+        //After Unity4,you have to do this function after WaitForEndOfFrame in Coroutine
+        //Or you will get the error:"ReadPixels was called to read pixels from system frame buffer, while not inside drawing frame"
+        zzTransparencyCaptureCustom.captureScreenshot("capture/capture" + currentFrame + ".png");
+        //파일 저장경로가 이상하다면 아래 사용
+        //zzTransparencyCapture.captureScreenshot(System.IO.Path.Combine(Application.dataPath, "Assets", "capture.png"));
+    }
+
+    void Start () {
+    //  StartCoroutine(CaptureEveryFrames());
+    }
+ 
+    IEnumerator CaptureEveryFrames () {
+        while(true){ // This creates a never-ending loop
+            yield return new WaitForEndOfFrame();
+            zzTransparencyCaptureCustom.captureScreenshot("capture/capture" + currentFrame + ".png");
+            currentFrame++;
+            Debug.Log("hi: " + currentFrame);
+        }
+    }
+    void Update()
+    {
+        zzTransparencyCaptureCustom.captureScreenshot("capture/capture" + currentFrame + ".png");
+        // if (Input.GetKeyDown(KeyCode.C))
+        // StartCoroutine(capture(currentFrame));
+        currentFrame++;
+    }
+}
