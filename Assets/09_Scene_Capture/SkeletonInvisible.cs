f42a933c4bf421cb7e2debe079dc93dcdbb64757
DAIN KIM <dain7672@gmail.com>
2020-07-25 21:42:53 +0900

    disable lighting and remove black color pixel to extract effect rendering only

---
+++ Assets/09_Scene_Capture/SkeletonInvisible.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SkeletonInvisible : MonoBehaviour
+{
+    private static string skeletonParticleName = "MotionParticle";
+    private List<GameObject> skeletonParticles = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        findAllChildrenClass(transform);
+        bool isOn = false;
+        foreach (GameObject particle in skeletonParticles) {
+            particle.GetComponent<MeshRenderer>().enabled = isOn;
+        }
+    }
+
+    void Update()
+    {
+        bool isOn = false;
+        foreach (GameObject particle in skeletonParticles) {
+            particle.GetComponent<MeshRenderer>().enabled = isOn;
+        }
+    }
+
+    private void findAllChildrenClass(Transform target) {
+        for (int i =0; i < target.childCount; i++) {
+            if (target.GetChild(i).name == skeletonParticleName) {
+                skeletonParticles.Add(target.GetChild(i).gameObject); //gameobject는 왜 transform으로부터 참조해야 할까.
+            }
+            findAllChildrenClass(target.GetChild(i));
+        }
+    }
+}
---
+++ Assets/09_Scene_Capture/TransparencyCapture_Custom/TransparencyCaptureToFileRemovingWhite.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class TransparencyCaptureToFileRemovingWhite:MonoBehaviour
+{
+    private int currentFrame = 0;
+    public IEnumerator capture(int currentFrame)
+    {
+
+        yield return new WaitForEndOfFrame();
+        //After Unity4,you have to do this function after WaitForEndOfFrame in Coroutine
+        //Or you will get the error:"ReadPixels was called to read pixels from system frame buffer, while not inside drawing frame"
+        zzTransparencyCaptureRemovingWhite.captureScreenshot("capture/capture" + currentFrame + ".png");
+        //파일 저장경로가 이상하다면 아래 사용
+        //zzTransparencyCapture.captureScreenshot(System.IO.Path.Combine(Application.dataPath, "Assets", "capture.png"));
+    }
+
+    void Start () {
+        StartCoroutine(CaptureEveryFrames());
+    }
+ 
+    IEnumerator CaptureEveryFrames () {
+        while(true){ // This creates a never-ending loop
+            yield return new WaitForEndOfFrame();
+            zzTransparencyCaptureRemovingWhite.captureScreenshot("capture/capture" + currentFrame + ".png");
+            Debug.Log("frame: " + currentFrame);
+            currentFrame++;
+            yield return null;
+        }
+    }
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.C))
+        // StartCoroutine(capture(currentFrame));
+        // capture(currentFrame);
+        // yield return null;
+        // currentFrame++;
+    }
+}
