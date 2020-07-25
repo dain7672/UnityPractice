06e465279663e45b6e7ec308e34436963f6f708c
DAIN KIM <dain7672@gmail.com>
2020-07-25 19:50:55 +0900

    sprial effect moving up-down each humanoid asset has different axis so couldn't apply it to mixamo humanoid

--- Assets/09_Scene_Capture/SprialMoving.cs
+++ Assets/09_Scene_Capture/SprialMoving.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 
 public class SprialMoving : MonoBehaviour
 {
-    public GameObject object1;
+    public GameObject object1; //1->2 방향으로 움직인다.
     public GameObject object2;
-    public bool moveUpward = false;
-    public float movingSpeed = 0.1f;
+    public bool moveUpDown = true;
+    public bool movingDirection = true; //if true, up.
+    public float radius = 0.15f;
 
-    public float relative = 0.5f;
+    public float rotateSpeed = 4;
     private Vector3 position;
-    private Vector3 vector;
+    private Vector3 rotateVector;
 
 
     void Start()
     {
-        transform.position = Vector3.Lerp(object1.transform.position, object2.transform.position, 0.5f);
-        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 0.15f, transform.localPosition.z);
-        
+        transform.position = Vector3.Lerp(object1.transform.position, object2.transform.position, 0.1f);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + radius, transform.localPosition.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        position = Vector3.Lerp(object1.transform.position, object2.transform.position, 0.5f);
-        vector =  object1.transform.position - object2.transform.position;
-        transform.RotateAround(position, vector, 4);        
+        position = Vector3.Lerp(object1.transform.position, object2.transform.position, 0.1f);
+        rotateVector =  object1.transform.position - object2.transform.position;
         
-        
-        // transform.position = Vector3.MoveTowards(object1.transform.position, object2.transform.position, 0.1f);
-        
+        transform.RotateAround(position, rotateVector, rotateSpeed);       
+        if (moveUpDown) {
+            if (isAfter(transform, object2.transform)) {
+                movingDirection = true;
+            } else if (isAfter(object1.transform, transform)) {
+                movingDirection = false;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + rotateVector * convertBoolToPositiveOrNegative(movingDirection), 0.002f);
+        }
+    }
+
+    private bool isBefore(Transform target, Transform current) {
+        Vector3 forward = current.TransformDirection(Vector3.right);
+        Vector3 toOther = target.position - current.position;
+
+        return Vector3.Dot(forward, toOther) < 0;
+    }
+
+    private bool isAfter(Transform target, Transform current) {
+        return isBefore(current, target);
+    }
+
+    private int convertBoolToPositiveOrNegative(bool boolean) {
+        return (-1 + 2 * Convert.ToInt16(boolean));
     }
 }
