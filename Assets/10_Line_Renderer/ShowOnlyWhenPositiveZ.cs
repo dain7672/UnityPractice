f52acb3be651a7724cebd66687721501732775d3
DAIN KIM <dain7672@gmail.com>
2020-08-05 23:07:04 +0900

    line renderer redering turn on off test

---
+++ Assets/10_Line_Renderer/ShowOnlyWhenPositiveZ.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOnlyWhenPositiveZ : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.forceRenderingOff = transform.position.z < 0;
+    }
+}
